b4717fceaa33d079277e1b8d895708bfb2c65a54
Laby Damaro Camara <ldamaro98@gmail.com>
2021-11-08 03:36:38 +0100

    review

---
+++ TP/review.sql
@@ -0,0 +1,5 @@
+--BACKUP DATABASE gescom TO [unite_logique]
+--BACKUP DATABASE gescom FILEGROUP='DATA' TO [unite_logique]
+--BACKUP LOG gescom TO [unite_logique]
+
+BACKUP DATABASE gescom FILEGROUP='DATA' TO [unite_logique] WITH DIFFERENTIAL
