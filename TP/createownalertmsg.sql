5bd9e4e709672191043348adc388dc46e629d15f
Laby Damaro Camara <ldamaro98@gmail.com>
2021-11-14 00:16:26 +0100

    affichage de msg personnalisé

--- TP/createownalertmsg.sql
+++ TP/createownalertmsg.sql
@@ -2,3 +2,10 @@ EXEC sp_addmessage @msgnum=57778,
 @severity=19,
 @msgtext='Log transaction FULL',
 @lang='us_english'
+
+EXEC sp_addmessage @msgnum=57778,
+@severity=19,
+@msgtext='Journal de transaction COMPLET',
+@lang='French'
+
+SELECT * FROM sysmessages WHERE error=57778
